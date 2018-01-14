619c985736a368577d23af7a2081323454973997
ArranGravestock <arran@gravestock.net>
2018-01-14 23:14:39 +0000

    Fix navigation transition

--- client/src/controls.js
+++ client/src/controls.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './controls.css';
 import Nav from './nav';
+import {Link } from 'react-router-dom';
 
 class Controls extends Component {
 
@@ -26,7 +27,7 @@ class Controls extends Component {
                 <li onClick={this.handleClick}>
                     <a>{this.state.toggled ? 'close' : 'menu'}</a>
                 </li>
-                <li><a>settings</a></li>
+                <li><Link to="/settings"><a>settings</a></Link></li>
             </ul>
             <Nav className={`nav-hidden-${this.state.toggled}`} onClick={this.handleClick}/>
         </div>
