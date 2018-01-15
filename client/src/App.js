a40f9db8c7ee45c9edd454f601014ed3e85c86a7
ArranGravestock <arran@gravestock.net>
2018-01-15 21:34:36 +0000

    Fix redundant imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Controls from './controls';
-import Wrapper from './wrapper';
 import LoginForm from './login';
-import {BrowserRouter as Router, Route, Redirect, withRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const App = () => (
   <Router>
