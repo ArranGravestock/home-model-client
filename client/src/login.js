2292e50ccbb0732381b99d3558b00ab1d9ea9937
ArranGravestock <arran@gravestock.net>
2018-01-16 16:58:21 +0000

    Add styles for login form

--- client/src/login.js
+++ client/src/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link, Redirect} from 'react-router-dom';
 import {fakeAuth} from './privateroute';
+import './login.css'
 
 class LoginForm extends Component {
     state = {
@@ -26,24 +27,22 @@ class LoginForm extends Component {
         }
 
         return(
-        
-        <div className="card">
-        <h1>login</h1>
-        <div>
-        <img src="https://placeimg.com/150/150/any" alt="placeholder"/>
-        </div>
-        <form className="login-controls">
-        <label>username
-        <br/>
-        <input placeholder="Username"/><br/>
-        </label>
-        
-        <label>password
-        <br/>
-        <input placeholder="Password"/><br/>
-        </label>
-        <button onClick={this.login}><Link to="/auth">login</Link></button>
-        </form>
+        <div className="login-flex">
+            <div className="card login">
+                <h1>login</h1>
+                    <form className="login-controls">
+                        <label>email</label>
+                        <br/>
+                        <input type="text" placeholder="john@doe.com"/><br/>
+                        
+                        
+                        <label>password</label>
+                        <br/>
+                        <input type="password"/><br/>
+                        
+                    <button onClick={this.login}><Link to="/auth">login</Link></button>
+                </form>
+            </div>
         </div>
         )
     }
